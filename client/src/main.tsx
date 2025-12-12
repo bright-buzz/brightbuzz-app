b4b0156c10497428c3dd637aa3013cc44f6e71e5
bright-buzz <hello@bright-buzz.com>
2025-12-12 13:47:33 -0500

    Add redirect URLs to ClerkProvider in main.tsx
    Adding home

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,11 @@ import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 createRoot(document.getElementById("root")!).render(
-  <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
-    <App />
-  </ClerkProvider>
+<ClerkProvider
+  publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+  afterSignInUrl="/home"
+  afterSignUpUrl="/home"
+>
+  <App />
+</ClerkProvider>
 );
